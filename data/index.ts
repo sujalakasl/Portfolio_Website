18c95398dfb487d96aa1e634efc86b7caeff12f5
Sujalakasl <sujallamichhaneakasl@gmail.com>
2025-02-15 01:52:09 +0545

    changes to data in index.jsx

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Currently building a React Native manga app",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -72,68 +72,53 @@ export const navItems = [
   export const projects = [
     {
       id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "/ui.earth.com",
+      title: "Movie search app with trending row",
+      des: "Search for the latest movies and discover trending movies.",
+      img: "/moviepic.PNG",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/fm.svg"],
+      link: "https://github.com/sujalakasl/Movie_app",
     },
     {
       id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "/ui.yoom.com",
+      title: "Real estate app",
+      des: "Look for your ideal propery with detailed descriptions and reviews.",
+      img: "/app.PNG",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+      link: "https://github.com/sujalakasl/Restap",
     },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "/ui.aiimg.com",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "/ui.apple.com",
-    },
+
   ];
   
   export const testimonials = [
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "It was a true pleasure working with Sujal. His professionalism, quick response time, and commitment to producing outstanding results shone through every step of the project. Sujal's passion for all aspects of development is truly remarkable. If you're looking to enhance your website and strengthen your brand, Sujal is the perfect choice.",
       name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+      title: "Director of *** Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "It was a true pleasure working with Sujal. His professionalism, quick response time, and commitment to producing outstanding results shone through every step of the project. Sujal's passion for all aspects of development is truly remarkable. If you're looking to enhance your website and strengthen your brand, Sujal is the perfect choice.",
       name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+      title: "Director of *** Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "It was a true pleasure working with Sujal. His professionalism, quick response time, and commitment to producing outstanding results shone through every step of the project. Sujal's passion for all aspects of development is truly remarkable. If you're looking to enhance your website and strengthen your brand, Sujal is the perfect choice.",
       name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+      title: "Director of *** Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "It was a true pleasure working with Sujal. His professionalism, quick response time, and commitment to producing outstanding results shone through every step of the project. Sujal's passion for all aspects of development is truly remarkable. If you're looking to enhance your website and strengthen your brand, Sujal is the perfect choice.",
       name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+      title: "Director of *** Technologies",
     },
     {
       quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        "It was a true pleasure working with Sujal. His professionalism, quick response time, and commitment to producing outstanding results shone through every step of the project. Sujal's passion for all aspects of development is truly remarkable. If you're looking to enhance your website and strengthen your brand, Sujal is the perfect choice.",
       name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
+      title: "Director of *** Technologies",
     },
   ];
   
@@ -173,29 +158,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Title Placeholder 1",
+      desc: "Placeholder desc 1.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
+      title: "Title Placeholder 2",
+      desc: "Placeholder desc 2.",
+      className: "md:col-span-2", 
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
+      title: "Title Placeholder 3",
+      desc: "Placeholder desc 3.",
+      className: "md:col-span-2", 
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Title Placeholder 4",
+      desc: "Placeholder desc 4.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
